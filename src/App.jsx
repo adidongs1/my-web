9687cebfcec9d46dc2d8ff306572ed17afbb1902
adidongs1 <adidongs15@gmail.com>
2024-02-03 18:49:49 +0700

    Add routes for admin login and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 
 import Maintenance from './pages/Maintenance'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import NotFound from './pages/NotFound'
+import SinglePost from './pages/SinglePost'
+import LoginAdmin from './components/LoginAdmin'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Maintenance />} />
-          <Route path="/portfolio" element={<Maintenance />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contacts" element={<Maintenance />} />
+        <AuthProvider>
+          <Routes>
+            <Route path="/admin" element={<LoginAdmin />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<Maintenance />} />
+            <Route path="/portfolio" element={<Maintenance />} />
+            <Route path="/contacts" element={<Maintenance />} />
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+            {/* path blogging */}
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/single-post/:id" element={<SinglePost />} />
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+
+        </AuthProvider>
       </Router>
     </>
   )
