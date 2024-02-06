a28ad99e61a3f3260db8580eb23d28e271c923a6
adidongs1 <adidongs15@gmail.com>
2024-02-06 12:04:37 +0700

    Refactor routing and add toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
+import { ToastContainer } from 'react-toastify'
 
+//pages
 import Maintenance from './pages/Maintenance'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import NotFound from './pages/NotFound'
 import SinglePost from './pages/SinglePost'
-import LoginAdmin from './components/LoginAdmin'
+import LoginAdmin from './pages/LoginAdmin'
 import Dashboard from './pages/Dashboard'
+
+//services
+import AuthProvider from './services/AuthProvider'
+import PrivateRoute from './services/PrivateRoute'
+import BlogProvider from './services/BlogProvider'
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Router>
         <AuthProvider>
-          <Routes>
-            <Route path="/admin" element={<LoginAdmin />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<Maintenance />} />
-            <Route path="/portfolio" element={<Maintenance />} />
-            <Route path="/contacts" element={<Maintenance />} />
+          <BlogProvider>
+            <Routes>
+              {/* admin */}
+              <Route path="/admin" element={<LoginAdmin />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/admin/dashboard" element={<Dashboard />} />
+              </Route>
 
-            {/* path blogging */}
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/single-post/:id" element={<SinglePost />} />
 
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<Maintenance />} />
+              <Route path="/portfolio" element={<Maintenance />} />
+              <Route path="/contacts" element={<Maintenance />} />
 
+              {/* path blogging */}
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/blog/single-post/:id" element={<SinglePost />} />
+
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BlogProvider>
         </AuthProvider>
       </Router>
     </>
