451cd74e7b322bc3da3b429c79c71d615ba5d5e8
adidongs1 <adidongs15@gmail.com>
2024-02-24 03:09:27 +0700

    Add new utility files for authentication and routing

---
+++ src/utils/AuthProvider.jsx
@@ -0,0 +1,95 @@
+import { createContext, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+
+export const AuthContext = createContext()
+
+export default function AuthProvider({ children }) {
+    const [user, setUser] = useState(null)
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            setUser({ token })
+        }
+    }, [])
+
+    const login = async (username, password) => {
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        }
+        try {
+            const response = await fetch(`${import.meta.env.VITE_BASE_URL}/wp-json/jwt-auth/v1/token`, config)
+            const data = await response.json()
+
+            if (response.ok) {
+                console.log(data)
+                setUser(data)
+                localStorage.setItem('token', data.token)
+
+                // Alert Berhasil
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Berhasil!',
+                    text: 'Selamat Datang!',
+                    showConfirmButton: true,
+                    confirmButtonText: 'OK',
+                    confirmButtonColor: '#17b472'
+
+                })
+                navigate('/admin/dashboard')
+                return
+
+            } else {
+                const errMessage = data.message ? data.message : 'Terjadi Kesalahan'
+
+                console.log(data.message)
+                // alert(data.errors.message)
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    html: errMessage,
+                    showConfirmButton: true,
+                    confirmButtonText: 'OK',
+                    confirmButtonColor: '#17b472'
+
+                })
+
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+
+    }
+
+    const logout = () => {
+        setUser(null)
+        localStorage.removeItem('token')
+        Swal.fire({
+            icon: 'success',
+            title: 'Logout Berhasil!',
+            text: 'Anda Berhasil Logout!',
+            showConfirmButton: true,
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#17b472'
+
+        })
+        navigate('/')
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
+
