a08e9632f81563e07c925a248e5f3891c955fda2
adidongs1 <adidongs15@gmail.com>
2024-02-28 19:35:55 +0700

    Add loading state to handlePage function

--- src/components/Paginations.jsx
+++ src/components/Paginations.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import chevronLeft from '../assets/icons/chevron-left.svg'
 import chevronRight from '../assets/icons/chevron-right.svg'
 
 function Paginations({ page, setPage, totalPages }) {
 
+    const [isLoading, setIsLoading] = useState(false)
+
     const handlePage = (number) => {
-        setPage(number)
-        window.scrollTo(0, 0)
+        setIsLoading(true)
+        setTimeout(() => {
+            setPage(number)
+            window.scrollTo(0, 0)
+            setIsLoading(false)
+        }, 2500)
+
     }
 
     const pageNumbers = []
